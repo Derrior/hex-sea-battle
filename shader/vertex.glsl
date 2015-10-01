936ee7a18775c81d8cf8457ce71d3fcec94ece68
Leo-nid <s19b2_ichalov@179.ru>
2015-10-01 19:11:02 +0300

    Update vertex.glsl

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,5 +1,5 @@
 attribute vec2 v_coord;
 
 void main(void) {
-    gl_Position = vec4(v_coord.x / (13.66 * 2) - 0.5, v_coord.y / (7.68 * 2) - 0.5, 1, 1); 
+    gl_Position = vec4(v_coord.x / 683 - 1, v_coord.y / 384 - 1, 1, 1); 
 }
